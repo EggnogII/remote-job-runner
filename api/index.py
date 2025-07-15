39c0134ba3f16c02ba6872d650b4299755b18096
Imran Irfan <imranirfan@MiniM4-2.local>
2025-07-15 14:32:37 -0700

    Rename some stuff

--- api/index.py
+++ api/index.py
@@ -2,22 +2,25 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-commands = [
-    { 'command': 'ls'}
+jobs = [
+    {   'id': "1",
+        'command': 'ls',
+        'status': 'queued'
+    }
 ]
 
 @app.route("/")
 def ping():
     return "I am alive!"
 
-@app.route("/submit", methods=['POST'])
+@app.route("/jobs", methods=['POST'])
 def submit_command():
-    commands.append(request.get_json())
+    jobs.append(request.get_json())
     return '', 200
 
-@app.route("/command_list")
+@app.route("/jobs")
 def get_command_list():
-    return f'{commands}', 200
+    return f'{jobs}', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
