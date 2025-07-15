4bda14c67a29f6d55bbaf970f5f069cecfeecc20
Imran Irfan <imranirfan@MiniM4-2.local>
2025-07-15 16:56:05 -0700

    Update index to indicate creation status, and output UUID's for created jobs

--- api/index.py
+++ api/index.py
@@ -13,11 +13,14 @@ def ping():
 def submit_command():
     new_job = Job.to_dict(None,job_details=request.get_json())
     jobs.append(new_job)
-    return '', 200
+    return 'created', 200
 
 @app.route("/jobs")
 def get_command_list():
-    return f'{jobs}', 200
+    job_id_list = []
+    for job in jobs:
+        job_id_list.append(job.id)
+    return f'{job_id_list}', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
