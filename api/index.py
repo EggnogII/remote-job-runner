96731dd8d60e26212a27d63e4e715eb8135adacf
Imran Irfan <imranirfan@MiniM4-2.local>
2025-07-14 20:33:50 -0700

    create a basic data structure to contain the command list

--- api/index.py
+++ api/index.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+commands = [
+    { 'command': 'ls'}
+]
 
 @app.route("/")
 def ping():
     return "I am alive!"
+
+@app.route("/submit", methods=['POST'])
+def submit_command():
+    commands.append(request.get_json())
+    return '', 200
+
+@app.route("/command_list")
+def get_command_list():
+    return f'{commands}', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
