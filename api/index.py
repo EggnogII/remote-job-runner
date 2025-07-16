e8dc586518cd38346533d177830e027933f6c76f
Imran Irfan <imranirfan@MiniM4-2.local>
2025-07-15 18:27:18 -0700

    Refactor to use Redis, first pass

--- api/index.py
+++ api/index.py
@@ -1,9 +1,11 @@
-import sys
+import redis
+import json
 from flask import Flask, jsonify, request
-from api.model.job import Job, Language, JobStatus
+from model.job import Job, Language, JobStatus
 app = Flask(__name__)
 
 jobs = []
+redis_client = redis.Redis()
 
 @app.route("/")
 def ping():
@@ -11,9 +13,17 @@ def ping():
 
 @app.route("/jobs", methods=['POST'])
 def submit_command():
-    new_job = Job.to_dict(None,job_details=request.get_json())
+    new_job = Job.to_request(None,job_details=request.get_json())
     jobs.append(new_job)
+    redis_client.set(f"{new_job.id}", json.dumps(new_job.to_dict()))
     return 'created', 200
+
+@app.route("/job/<job_id>", methods=['GET'])
+def get_job(job_id):
+    key = job_id
+    job_raw_data = redis_client.get(key)
+    job_data = json.loads(job_raw_data)
+    return jsonify(job_data), 200
 
 @app.route("/jobs")
 def get_command_list():
