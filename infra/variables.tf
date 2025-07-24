6e616f17cbbe1a84b7efe379aad60818665498f0
Imran Irfan <imranirfan@MiniM4-2.local>
2025-07-24 14:46:41 -0700

    Updated variables for the vpc and subnets

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,15 @@
-variable redis_cluster_password {
+variable "redis_cluster_password" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_b_id" {
+  type = string
+}
+
+variable "subnet_c_id" {
     type = string
 }
